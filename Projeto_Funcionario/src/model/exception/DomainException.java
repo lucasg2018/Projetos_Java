3c219fb90e6f4efcf019f35189d0301133742b86
Lucas Gabriel de Lima Souza <lucasg2018@outlook.com>
2020-05-26 11:35:50 -0300

    x

--- Projeto_Funcionario/src/model/exception/DomainException.java
+++
@@ -1,7 +0,0 @@
-package model.exception;
-
-public class DomainException extends RuntimeException{
-	public DomainException(String msg) {
-		super(msg);
-	}
-}
--- Teste/src/program/Main.java
+++ Teste/src/program/Main.java
@@ -2,23 +2,50 @@ package program;
 
 import java.util.Scanner;
 
+import exception.DbException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		double pedagio = 300;
-		
-		System.out.print("Informe a quantidade de dias úteis: ");
-		int diasU = ler.nextInt();
-		System.out.print("Informe a quantidade de dias não úteis: ");
-		int diasNU = ler.nextInt();
-
-		double valNU = pedagio / ((diasU * 0.66) + diasNU);
-		double valU = valNU * 0.66;
-		
-		System.out.printf("Valor do pedágio em dias úteis: %.2f reais.", valU);
-		System.out.printf("\nValor do pedágio em dias não úteis: %.2f reais.", valNU);
+		try {
+			System.out.print("Data de nascimento(DD/MM/YYYY): ");
+			String birthDate = ler.nextLine();
+			
+			System.out.println(birthDate.length());
+			
+			if (birthDate.length() != 10) {
+				System.out.println("Erro! Formato da data incorreto");
+			} else {
+				validacaoDate(birthDate);
+			}	
+			
+			System.out.println("Tudo ok!");
+			
+		}catch(DbException e) {
+			System.out.println(e.getMessage());
+		}catch(RuntimeException e) {
+			System.out.println("Erro inesperado!");
+		}
 		
 		ler.close();
+	}
+
+	private static void validacaoDate(String birthDate) {
+		try {
+			int dia = Integer.parseInt(birthDate.substring(0, 2));
+			int mes = Integer.parseInt(birthDate.substring(3, 5));
+			int ano = Integer.parseInt(birthDate.substring(6, 10));
+
+			if (dia > 31 || dia < 1) {
+				throw new DbException("Erro! Dia inválido!");
+			} else if (mes > 12 || mes < 1) {
+				throw new DbException("Erro! Mês inválido!");
+			} else if (ano < 1900) {
+				throw new DbException("Erro! Ano inválido!");
+			}
+		} catch (Exception e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 }
